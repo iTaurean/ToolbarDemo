4b8be6d97ae20dd842a2a6ccadf812fed3b4d42c
Alex Lv <lvxineye@gmail.com>
2015-12-29 09:57:05 +0800

    Toolbar demo
    - Toolbar的使用
    - Toolbar作为项目的基类的使用

---
+++ app/src/main/java/com/android/lvxin/activity/BaseToolbarActivity.java
@@ -0,0 +1,15 @@
+package com.android.lvxin.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.android.lvxin.R;
+
+public class BaseToolbarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base_with_toolbar);
+    }
+}
---
+++ app/src/main/java/com/android/lvxin/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.android.lvxin.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.lvxin.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
